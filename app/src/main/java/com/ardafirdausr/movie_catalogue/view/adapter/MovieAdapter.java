8fc0109d9ee81cd2eb452aee28778b9e18bebd8e
Arda <ardafirdausr@gmail.com>
2020-01-23 17:50:34 +0700

    move MovieListActivity to MoviesFragment

--- app/src/main/java/com/ardafirdausr/movie_catalogue/view/adapter/MovieAdapter.java
+++ app/src/main/java/com/ardafirdausr/movie_catalogue/view/adapter/MovieAdapter.java
@@ -1,12 +1,14 @@
 package com.ardafirdausr.movie_catalogue.view.adapter;
 
-import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ardafirdausr.movie_catalogue.R;
 import com.ardafirdausr.movie_catalogue.api.movie.response.Movie;
@@ -15,80 +17,69 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MovieAdapter extends BaseAdapter {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
-    private Context context;
     private List<Movie> movies = new ArrayList<>();
     private OnItemClickCallback onItemClickCallback;
-
-    public void setOnItemClickCallback(OnItemClickCallback onItemClickCallback) {
-        this.onItemClickCallback = onItemClickCallback;
-    }
 
     public void setMovies(List<Movie> movies) {
         this.movies = movies;
     }
 
-    public MovieAdapter(Context context) {
-        this.context = context;
+    public void setOnItemClickCallback(OnItemClickCallback onItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie_list, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public int getCount() {
-        return movies.size();
-    }
-
-    @Override
-    public Movie getItem(int position) {
-        return movies.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return movies.get(position).getId();
-    }
-
-    @Override
-    public View getView(int position, View itemView, ViewGroup parent) {
-        if (itemView == null) {
-            itemView = LayoutInflater.from(context).inflate(R.layout.item_movie_list, parent, false);
-        }
-        ViewHolder viewHolder = new ViewHolder(itemView);
-        final Movie movie = getItem(position);
-        viewHolder.bind(movie);
-        viewHolder.vgListitem.setOnClickListener(new View.OnClickListener() {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final Movie movie = movies.get(position);
+        holder.bind(movie);
+        holder.vgListitem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onItemClickCallback.onClick(movie);
             }
         });
-        return itemView;
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
     }
 
     public interface OnItemClickCallback {
         void onClick(Movie movie);
     }
 
-    private class ViewHolder {
-        public ViewGroup vgListitem;
-        public TextView tvTitle, tvRating, tvDescription;
-        public ImageView ivPoster;
 
-        ViewHolder(View view) {
-            vgListitem = view.findViewById(R.id.vg_list_item);
-            tvTitle = view.findViewById(R.id.tv_title);
-            tvRating = view.findViewById(R.id.tv_rating);
-            tvDescription= view.findViewById(R.id.tv_description);
-            ivPoster = view.findViewById(R.id.iv_poster);
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private ViewGroup vgListitem;
+        private TextView tvTitle, tvRating, tvDescription;
+        private ImageView ivPoster;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            vgListitem = itemView.findViewById(R.id.vg_list_item);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+            tvRating = itemView.findViewById(R.id.tv_rating);
+            tvDescription= itemView.findViewById(R.id.tv_description);
+            ivPoster = itemView.findViewById(R.id.iv_poster);
         }
 
         void bind(Movie movie) {
+            String rating = movie.getVote() + " / " + 10;
             tvTitle.setText(movie.getTitle());
-            tvRating.setText(movie.getVote() + " / " + 10);
+            tvRating.setText(rating);
             tvDescription.setText(movie.getDescription());
             Picasso.get().load(movie.getImageUrl()).into(ivPoster);
         }
     }
 
 }
-
--- app/src/main/java/com/ardafirdausr/movie_catalogue/view/fragment/MoviesFragment.java
+++ app/src/main/java/com/ardafirdausr/movie_catalogue/view/fragment/MoviesFragment.java
@@ -1,21 +1,47 @@
 package com.ardafirdausr.movie_catalogue.view.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.ardafirdausr.movie_catalogue.BuildConfig;
 import com.ardafirdausr.movie_catalogue.R;
+import com.ardafirdausr.movie_catalogue.api.movie.MovieApiClient;
+import com.ardafirdausr.movie_catalogue.api.movie.MovieApiInterface;
+import com.ardafirdausr.movie_catalogue.api.movie.response.Movie;
+import com.ardafirdausr.movie_catalogue.api.movie.response.MovieList;
+import com.ardafirdausr.movie_catalogue.view.activity.MovieDetailActivity;
+import com.ardafirdausr.movie_catalogue.view.adapter.MovieAdapter;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MoviesFragment extends Fragment {
+public class MoviesFragment extends Fragment implements View.OnClickListener{
 
+    private List<Movie> movies;
+    private RecyclerView rvMovies;
+    private TextView tvState;
+    private ProgressBar pbLoading;
+    private Button btRetry;
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -26,8 +52,112 @@ public class MoviesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_movies, container, false);
-        return root;
+        return inflater.inflate(R.layout.fragment_movies, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvMovies = view.findViewById(R.id.rv_movie_list);
+        tvState = view.findViewById(R.id.tv_state);
+        pbLoading = view.findViewById(R.id.pb_loading);
+        btRetry = view.findViewById(R.id.bt_retry);
+        btRetry.setOnClickListener(this);
+        fetchNowPlayingMovies();
+    }
+
+    private void fetchNowPlayingMovies(){
+        renderLoadingState();
+        MovieApiInterface movieApi = MovieApiClient.getClient().create(MovieApiInterface.class);
+        final Call<MovieList> movieListRequest = movieApi.getNowPlayingMovies(BuildConfig.MOVIE_DB_API_KEY, 1);
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        movieListRequest.enqueue(new Callback<MovieList>() {
+                            @Override
+                            public void onResponse(Call<MovieList> call, Response<MovieList> response) {
+                                if(response.isSuccessful()){
+                                    movies = response.body().getMovies();
+                                    if(movies.size() > 0){
+                                        renderPopulatedMovieListState();
+                                    }
+                                    else {
+                                        renderEmptyMovieListState();
+                                    }
+                                }
+                                else {
+                                    renderFailedFetch();
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<MovieList> call, Throwable t) {
+                                renderFailedFetch();
+                            }
+                        });
+                    }
+                },
+                1200);
+    }
+
+    private void showMovieList(){
+        MovieAdapter movieAdapter = new MovieAdapter();
+        movieAdapter.setMovies(movies);
+        movieAdapter.setOnItemClickCallback(new MovieAdapter.OnItemClickCallback() {
+            @Override
+            public void onClick(Movie movie) {
+                navigateToMovieDetail(movie);
+            }
+        });
+        rvMovies.setAdapter(movieAdapter);
+        rvMovies.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    private void navigateToMovieDetail(Movie movie){
+        Intent navigateToMovieDetailIntent = new Intent(getContext(), MovieDetailActivity.class);
+        navigateToMovieDetailIntent.putExtra(MovieDetailActivity.EXTRA_MOVIE, movie);
+        startActivity(navigateToMovieDetailIntent);
+    }
+
+    private void hideAllViews(){
+        tvState.setVisibility(View.INVISIBLE);
+        rvMovies.setVisibility(View.INVISIBLE);
+        pbLoading.setVisibility(View.INVISIBLE);
+        btRetry.setVisibility(View.INVISIBLE);
+    }
+
+    private void renderLoadingState(){
+        hideAllViews();
+        tvState.setText(R.string.loading);
+        tvState.setVisibility(View.VISIBLE);
+        pbLoading.setVisibility(View.VISIBLE);
+    }
+
+    private void renderEmptyMovieListState(){
+        hideAllViews();
+        tvState.setText(R.string.no_data_displayed);
+        tvState.setVisibility(View.VISIBLE);
+    }
+
+    private void renderPopulatedMovieListState(){
+        hideAllViews();
+        showMovieList();
+        rvMovies.setVisibility(View.VISIBLE);
+    }
+
+    private void renderFailedFetch(){
+        hideAllViews();
+        tvState.setText(R.string.fetch_data_failed);
+        tvState.setVisibility(View.VISIBLE);
+        btRetry.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bt_retry:
+                fetchNowPlayingMovies();
+                break;
+        }
+    }
 }
